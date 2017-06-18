159f85108483dfe7d9e37ff2b075f6cc6f447fb2
brandonjfajardo <brandonjfajardo@hotmail.com>
2017-06-18 10:27:13 -0700

    Create skeleton

---
+++ src/components/todolist.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TodoList = () => {
+    return (
+        <div>
+            <h1>Todos</h1>
+            <input />
+            <button>Add</button>
+            <ul />
+        </div>
+    )
+}
+
+export default TodoList
